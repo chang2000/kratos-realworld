1c064a4852de029ca0d0565b185b86fea012fd4c
WANG Tianchang <wangtcalex@gmail.com>
2022-03-10 17:43:39 +0800

    add deploy

--- internal/data/data.go
+++ internal/data/data.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/go-kratos/kratos/v2/log"
 	"github.com/google/wire"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 // ProviderSet is data providers.
@@ -21,4 +23,17 @@ func NewData(c *conf.Data, logger log.Logger) (*Data, func(), error) {
 		log.NewHelper(logger).Info("closing the data resources")
 	}
 	return &Data{}, cleanup, nil
+}
+
+func NewDB(c *conf.Data) *gorm.DB {
+	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
+	dsn := "root:pass@tcp(127.0.0.1:3306)/realworld?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	if err := db.AutoMigrate(); err != nil {
+		panic(err)
+	}
+	return db
 }
